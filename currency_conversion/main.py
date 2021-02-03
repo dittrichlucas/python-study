4b36d9a895be2d6a8bec337a2d6a840246074a3f
lucas.dittrich <ld.dittrich@gmail.com>
2021-02-02 22:06:56 -0300

    refactor: update route and add list of improvements
    Signed-off-by: lucas.dittrich <ld.dittrich@gmail.com>

--- currency_conversion/main.py
+++ currency_conversion/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from conversion import conversion  # type: ignore
 
 app = FastAPI()
 
+"""
+PADRÃO DA ROTA:
+http://localhost:8000/exchange/{amount}/{from}/{to}/{rate}
+http://localhost:8000/exchange/10/BRL/USD/4.50
 
-@app.get('/')
-async def root():
-    return {'message': 'Hello world!'}
+TODO: gerar modelo para não passar muitos parâmetros
+TODO: desenvolver testes
+TODO: consumir dados de uma api pública para apresentar dados em tempo real
+"""
+
+
+@app.get('/exchange/{amount}/{from_to}/{to}/{rate}')
+async def root(amount: float, from_to: str, to: str, rate: float):
+    return conversion(amount, from_to, to, rate)
